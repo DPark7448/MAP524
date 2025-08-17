1594cea8c2bad4667600047427c3339a5ddd26dc
Daniel Park <113808750+DPark7448@users.noreply.github.com>
2025-08-17 15:50:54 -0300

    Add files via upload

---
+++ FinalExam-BookApp/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  ExamPart2
+//
+//  Created by Daniel Park on 2025-08-12.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
