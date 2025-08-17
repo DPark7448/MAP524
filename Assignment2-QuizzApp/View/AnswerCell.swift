13041999780c520529b27a7d9a803d740da2c002
Daniel Park <113808750+DPark7448@users.noreply.github.com>
2025-08-17 17:58:33 -0300

    Add files via upload

---
+++ Assignment2-QuizzApp/View/AnswerCell.swift
@@ -0,0 +1,12 @@
+//
+//  AnswerCell.swift
+//  QuizApp
+//
+//  Created by Daniel Park on 2025-07-04.
+//
+
+import UIKit
+
+class AnswerCell: UITableViewCell {
+    @IBOutlet weak var answerLabel: UILabel!
+}
